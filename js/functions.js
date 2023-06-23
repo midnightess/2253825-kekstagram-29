be81c6e230c15433cede72eb4fa862bee1ddb8a1
Midnightess <asm1@bk.ru>
2023-06-23 18:09:11 +0300

    Задание 2. Функции

--- js/functions.js
+++ js/functions.js
@@ -6,3 +6,33 @@ function checkStringLength(string, length) {
   return false;
 }
 
+// Палиндром
+function isPalindrom (string) {
+  const normalizedString = string.toLowerCase().replaceAll(' ', '');
+  const reversedString = normalizedString.split('').reverse().join('');
+
+  return normalizedString === reversedString;
+}
+
+// Извлечение чисел
+function extractNumber (string) {
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      result += string.at(i);
+    }
+  }
+  return parseInt(result, 10);
+}
+
+// Возвращает исходную строку
+function myPadStart (string, minlength, pad) {
+  const actualPad = minlength - string.length;
+
+  if (actualPad <= 0) {
+    return string;
+  }
+  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad
+    / pad.length) + string;
+}
+
