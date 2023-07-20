e90b6e5fc52abbbf196c3d36b91b6d2233571813
Midnightess <asm1@bk.ru>
2023-07-21 00:25:47 +0300

    Исправила на стрелочные функции

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,18 @@
-// Проверка длины строки (исправила return)
-function checkStringLength (string, length) {
-  return string.length <= length;
-}
+// Проверка длины строки
+const checkStringLength = (string, length) => string.length <= length;
 
 checkStringLength('проверяемая строка', 18);
 
-
-// Палиндром (убрала лишнюю переменную)
-function isPalindrom (string) {
+// Палиндром
+const isPalindrom = (string) => {
   const normalizedString = string.toLowerCase().replaceAll(' ', '');
-
   return normalizedString === normalizedString.split('').reverse().join('');
-}
+};
 
 isPalindrom('Лёша на полке клопа нашёл');
 
-
-// Извлечение чисел (понятный вариант)
-function extractNumber (string) {
+// Извлечение чисел
+const extractNumber = (string) => {
   let result = '';
   for (let i = 0; i < string.length; i++) {
     if (!Number.isNaN(parseInt(string.at(i), 10))) {
@@ -25,17 +20,15 @@ function extractNumber (string) {
     }
   }
   return parseInt(result, 10);
-}
+};
 
-extractNumber('ECMAScript 2022');
+extractNumber('ECMAScript 2023');
 
-
-/* Возвращает исходную строку (вариант 2, с помощью тернарного оператора
-  и стрелочной функции) */
+// Возвращает исходную строку
 const myPadStart = (string, minLength, pad) => {
   const actualPad = minLength - string.length;
-  return (actualPad <= 0) ? string : pad.slice(0, actualPad % pad.length)
-+ pad.repeat(actualPad / pad.length) + string;
+  return (actualPad <= 0) ? string : pad.slice (0, actualPad % pad.length)
++ pad.repeat (actualPad / pad.length) + string;
 };
 
 myPadStart('q', 4, 'werty');
