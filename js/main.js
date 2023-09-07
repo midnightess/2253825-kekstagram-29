eb9a8d2e99608106c84afc5bebee68ff38428b41
Midnightess <asm1@bk.ru>
2023-09-08 00:29:49 +0300

    Task 9

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 import { getPictures } from './data.js';
 import { renderGallery } from './gallery.js';
+import { setupForm } from './form.js';
+import './form.js';
+import './effect.js';
 
-renderGallery(getPictures());
+const pictureList = getPictures();
+renderGallery(pictureList);
+setupForm();
+
 
